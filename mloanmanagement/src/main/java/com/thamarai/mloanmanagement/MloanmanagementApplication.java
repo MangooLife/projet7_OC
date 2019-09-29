3c2b1873030597e9d86291d33571e018ba436ca1
MangooLife <writeandblack@msn.com>
2019-09-29 16:00:24 +0200

    init project

---
+++ mloanmanagement/src/main/java/com/thamarai/mloanmanagement/MloanmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.thamarai.mloanmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MloanmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MloanmanagementApplication.class, args);
+	}
+
+}
---
+++ mperson/src/main/java/com/thamarai/mperson/MpersonApplication.java
@@ -0,0 +1,13 @@
+package com.thamarai.mperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpersonApplication.class, args);
+	}
+
+}
